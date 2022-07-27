623a1d3632e878fb4741de2e2e3cdff878ef1ba6
KELLY LUU <kellyluuu@gmail.com>
2022-07-27 16:00:27 -0500

    Config deployment

--- lostandhound/settings.py
+++ lostandhound/settings.py
@@ -35,9 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms'
+    'crispy_forms',
     
-
+    'storages',
 ]
 
 MIDDLEWARE = [
